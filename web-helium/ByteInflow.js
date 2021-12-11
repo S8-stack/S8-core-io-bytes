85a95dfd1b0dbeacb6e8936716eab01b11dc9d9e
pierreconvert <pierre.convert@qxloop.com>
2021-12-11 23:36:15 +0100

    update

--- web-helium/ByteInflow.js
+++ web-helium/ByteInflow.js
@@ -19,7 +19,7 @@ export class ByteInflow {
 	 * 
 	 * @param {ArrayBuffer} arraybuffer 
 	 */
-	constructor(arraybuffer){
+	constructor(arraybuffer) {
 		this.arraybuffer = arraybuffer;
 		this.view = new DataView(arraybuffer);
 		this.textDecoder_ASCII = new TextDecoder('ascii');
@@ -27,81 +27,111 @@ export class ByteInflow {
 		this.offset = 0;
 	}
 
-	
-	getByte(){
+
+	/**
+	 * 
+	 * @returns 
+	 */
+	getByte() {
 		let value = this.view.getUint16(this.offset);
-		this.offset+=1;
+		this.offset += 1;
 		return value;
 	}
 
 
-	getUInt8(){
+	/**
+	 * 
+	 * @returns 
+	 */
+	getUInt8() {
 		let value = this.view.getUint8(this.offset);
-		this.offset+=1;
+		this.offset += 1;
 		return value;
 	}
 
-	getUInt16(){
+
+	/**
+	 * 
+	 * @returns 
+	 */
+	getUInt16() {
 		let value = this.view.getUint16(this.offset);
-		this.offset+=2;
+		this.offset += 2;
 		return value;
 	}
 
-	getInt16(){
+
+	/**
+	 * 
+	 * @returns 
+	 */
+	getInt16() {
 		let value = this.view.getInt16(this.offset);
-		this.offset+=2;
+		this.offset += 2;
 		return value;
 	}
 
-	getUInt32(){
+	getUInt32() {
 		let value = this.view.getUint32(this.offset);
-		this.offset+=4;
+		this.offset += 4;
 		return value;
 	}
 
-	getInt32(){
+	getInt32() {
 		let value = this.view.getInt32(this.offset);
-		this.offset+=4;
+		this.offset += 4;
 		return value;
 	}
 
-	getUInt64(){
+	getUInt64() {
 		let value = this.view.getUInt64(this.offset);
-		this.offset+=8;
+		this.offset += 8;
 		return value;
 	}
-	
-	getInt64(){
+
+	getInt64() {
 		let value = this.view.getInt64(this.offset);
-		this.offset+=8;
+		this.offset += 8;
 		return value;
 	}
 
-	getFloat32(){
+	getL8UInt(){
+        let script = "";
+        let length = this.view.getUint8(this.offset);
+		this.offset += 1;
+        for(let i=0; i<length; i++){
+            let digit = this.view.getUint8(this.offset);
+            this.offset += 1;
+            script += digit > 0x0f ? digit.toString(16) : '0'+digit.toString(16);
+        }
+        return script;
+    }
+
+	getFloat32() {
 		let value = this.view.getFloat32(this.offset);
-		this.offset+=4;
+		this.offset += 4;
 		return value;
 	}
 
-	getFloat64(){
+	getFloat64() {
 		let value = this.view.getFloat64(this.offset);
-		this.offset+=8;
+		this.offset += 8;
 		return value;
 	}
 
-	getL8StringASCII(){
+	getL8StringASCII() {
 		let length = this.getUInt8();
 		let stringView = new Uint8Array(this.arraybuffer, this.offset, length);
 		let value = this.textDecoder_ASCII.decode(stringView);
-		this.offset+=length;
+		this.offset += length;
 		return value;
 	}
 
-	getL32StringUTF8(){
+	getL32StringUTF8() {
 		let length = this.getUInt32();
 		let stringView = new Uint8Array(this.arraybuffer, this.offset, length);
 		let value = this.textDecoder_UTF8.decode(stringView);
-		this.offset+=length;
+		this.offset += length;
 		return value;
 	}
 
