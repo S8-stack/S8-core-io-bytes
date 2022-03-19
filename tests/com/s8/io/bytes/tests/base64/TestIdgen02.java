90ca8efd561934336d0fa9e735a15e5c8855b174
Pierre Convert <pierreconvert@macbook-pro-2.home>
2022-03-19 10:11:29 +0100

    update

--- tests/com/s8/io/bytes/tests/base64/TestIdgen02.java
+++ tests/com/s8/io/bytes/tests/base64/TestIdgen02.java
@@ -1,5 +1,7 @@
 package com.s8.io.bytes.tests.base64;
 
+import java.util.HashSet;
+
 import com.s8.io.bytes.base64.Base64Generator;
 
 public class TestIdgen02 {
@@ -13,11 +15,17 @@ public class TestIdgen02 {
 		System.out.println(generator.generate(108909860L));
 		System.out.println(generator.generate(7779760L));
 		
-		long code = 0x00L;
-		while(code < 0xfffffffL) {
-			System.out.println(generator.generate(code));
-			code+=1727;
+		
+		long index = 47;
+		
+		HashSet<String> set = new HashSet<String>();
+		for(int i=0; i<256000; i++) {
+			set.add(generator.generate(index++));
 		}
+		System.out.println("set: > "+set.size());
+		
+		
+		
 	}
 
 }
