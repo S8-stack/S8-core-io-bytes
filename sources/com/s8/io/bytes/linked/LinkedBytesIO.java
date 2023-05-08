fc4221094235cabcf74105d3693f44abe318053e
Pierre Convert <pierreconvert@mx1.asteas.com>
2023-05-08 18:42:47 +0200

    code clean-up

--- sources/com/s8/io/bytes/linked/LinkedBytesIO.java
+++ sources/com/s8/io/bytes/linked/LinkedBytesIO.java
@@ -7,7 +7,6 @@ import static java.nio.file.StandardOpenOption.WRITE;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -21,67 +20,20 @@ import java.nio.file.Path;
 public class LinkedBytesIO {
 	
 	
-	public static LinkedBytes read(Path path, boolean isVerbose) throws IOException {
-		return new LinkedBytesIO(path, isVerbose).read();
-	}
-	
-	
-	public static void write(LinkedBytes head, Path path, boolean isVerbose) throws IOException {
-		new LinkedBytesIO(path, isVerbose).write(head);
-	}
-	
-	
-
-	public final static int HEADER_BYTECOUNT = 10;
-
-	public final static byte[] HEADER_OPENING_TAG = "<:".getBytes(StandardCharsets.US_ASCII);
-
-	public final static byte[] HEADER_CLOSING_TAG = "/>".getBytes(StandardCharsets.US_ASCII);
-
-
-	/**
-	 * max nb attempts
-	 */
-	public final static int MAX_NB_ATTEMPTS = 256;
-	
-
-	/**
-	 * path
-	 */
-	private final Path path;
-
-
-	/**
-	 * is verbose
-	 */
-	private final boolean isVerbose;
-
-	
-	private FileChannel channel = null;
 
 	
 	
+	
+
 	/**
 	 * 
-	 * @param head
 	 * @param path
-	 * @param isVerbose
-	 * @throws IOException 
+	 * @return
+	 * @throws IOException
 	 */
-	private LinkedBytesIO(Path path, boolean isVerbose) {
-		super();
-		this.path = path;
-		this.isVerbose = isVerbose;
-	}
+	public static LinkedBytes read(Path path, boolean isVerbose) throws IOException {
 
-	
-	
-	
-	
-	
-
-	public LinkedBytes read() throws IOException {
-
+		FileChannel channel = null;
 		try {
 			// ensure directories are created
 			Path folderpath = path.getParent();
@@ -98,9 +50,13 @@ public class LinkedBytesIO {
 			boolean hasNext = true;
 			while(hasNext) {
 				
-				LinkedBytes chunk = parseHeader();
-				readBuffer(chunk.wrap());
-				hasNext = (chunk.attributes & 0x01) == 0x01;
+				LinkedBytesHeader header = LinkedBytesHeader.parse(channel);
+				
+				LinkedBytes chunk = new LinkedBytes(header.getLength());
+				
+				readBuffer(channel, chunk.wrap());
+				
+				hasNext = header.hasNext();
 				
 				if(head != null) { 
 					tail.next = chunk; tail = chunk;
@@ -133,66 +89,15 @@ public class LinkedBytesIO {
 
 
 
-	private LinkedBytes parseHeader() throws IOException {
-
-		ByteBuffer buffer = ByteBuffer.allocate(HEADER_BYTECOUNT);
-		readBuffer(buffer);
-		buffer.flip();
-		
-		/* <matching tag> */
-		byte[] tag = new byte[HEADER_OPENING_TAG.length];
-		buffer.get(tag);
-		if(tag[0]!=HEADER_OPENING_TAG[0] || tag[1]!=HEADER_OPENING_TAG[1]) {
-			throw new IOException("Failed to match HEADER_OPENING_TAG");
-		}
-		/* </matching tag> */
-		
-		short attributes = buffer.getShort();
-		
-		int length = buffer.getInt();
-		
-		/* <matching tag> */
-		tag = new byte[HEADER_CLOSING_TAG.length];
-		buffer.get(tag);
-		if(tag[0]!=HEADER_CLOSING_TAG[0] || tag[1]!=HEADER_CLOSING_TAG[1]) {
-			throw new IOException("Failed to match HEADER_OPENING_TAG");
-		}
-		/* </matching tag> */
-		
-		LinkedBytes chunk = new LinkedBytes(length);
-		chunk.attributes = attributes;
-		
-		return chunk;
-	}
-
-
-	/**
-	 * 
-	 */
-	private void readBuffer(ByteBuffer buffer) throws IOException {
-		
-		// attempts count
-		int c = 0;
-
-		// must at least flush half of the buffer
-		while(buffer.hasRemaining() && c < MAX_NB_ATTEMPTS) {
-			channel.read(buffer);
-			c++;
-		}
-
-		if(c == MAX_NB_ATTEMPTS) {
-			throw new IOException("Nb of retrieve attempts exceed limit");
-		}
-	}
-	
-
 	
 	/**
 	 * 
 	 * @return
 	 * @throws IOException 
 	 */
-	public void write(LinkedBytes head) throws IOException {
+	public static void write(LinkedBytes head, Path path, boolean isVerbose) throws IOException {
+		
+		FileChannel channel = null;
 		
 		try {
 			// ensure directories are created
@@ -210,13 +115,15 @@ public class LinkedBytesIO {
 			LinkedBytes chunk = head;
 			// is sending
 			while(chunk != null) {
-				ByteBuffer buffer = chunk.wrap();
 				
+
 				// write header
-				pushHeader(chunk);
+				LinkedBytesHeader header = new LinkedBytesHeader(chunk.next != null, chunk.length);
+				header.compose(channel);
 				
 				// write body
-				writeBuffer(buffer);
+				ByteBuffer buffer = chunk.wrap();
+				writeBuffer(channel, buffer);
 
 				// move to next chunk
 				chunk = chunk.next;
@@ -237,36 +144,49 @@ public class LinkedBytesIO {
 	}
 
 	
+
+
+	/**
+	 * max nb attempts
+	 */
+	public final static int MAX_NB_ATTEMPTS = 256;
+	
+	
+	
+	
+
+	/**
+	 * 
+	 * @param channel
+	 * @param buffer
+	 * @throws IOException
+	 */
+	public static void readBuffer(FileChannel channel, ByteBuffer buffer) throws IOException {
+		
+		// attempts count
+		int c = 0;
+
+		// must at least flush half of the buffer
+		while(buffer.hasRemaining() && c < MAX_NB_ATTEMPTS) {
+			channel.read(buffer);
+			c++;
+		}
+
+		if(c == MAX_NB_ATTEMPTS) {
+			throw new IOException("Nb of retrieve attempts exceed limit");
+		}
+	}
+	
+
 	
 	
 	/**
 	 * 
-	 * @param head
-	 * @return
+	 * @param channel
+	 * @param buffer
 	 * @throws IOException
 	 */
-	private void pushHeader(LinkedBytes chunk) throws IOException {
-
-		short flags = 0x00;
-		if(chunk.next!=null) {
-			flags |= 0x01;
-		}
-		
-		ByteBuffer header = ByteBuffer.allocate(HEADER_BYTECOUNT);
-		header.put(HEADER_OPENING_TAG);
-		header.putShort(flags);
-		header.putInt(chunk.length);
-		header.put(HEADER_CLOSING_TAG);
-		
-		header.flip();
-		
-		writeBuffer(header);
-	}
-	
-	
-	
-	
-	private void writeBuffer(ByteBuffer buffer) throws IOException {
+	public static void writeBuffer(FileChannel channel, ByteBuffer buffer) throws IOException {
 		
 		int iAttempt = 0;
 
