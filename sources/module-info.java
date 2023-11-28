1fb35cabbc14914a59ea85e264666d78a4450086
Pierre Convert <pierreconvert@macbook-pro-6.home>
2023-11-28 06:09:17 +0100

    Update

--- sources/module-info.java
+++ sources/module-info.java
@@ -5,28 +5,37 @@
  * Copyright (C) 2022, Pierre Convert. All rights reserved.
  *
  */
+
+import com.s8.api.meta.S8Dependency;
+
+
+@S8Dependency(jar = "")
 module com.s8.core.io.bytes {
-	
+
+
 
 	exports com.s8.core.io.bytes;
 	exports com.s8.core.io.bytes.linked;
-	
-	
+
+
 	// others
 	exports com.s8.core.io.bytes.base64;
 	exports com.s8.core.io.bytes.utilities.index;
 	exports com.s8.core.io.bytes.utilities.reactive;
 	exports com.s8.core.io.bytes.utilities.log;
 	exports com.s8.core.io.bytes.utilities.others;
+
+
+	exports com.s8.blocks.bytes.demos.d0;
 	
-	
+
+	requires transitive com.s8.api;
+
+
 	/*
 	 * dependencies: none
 	 */
-	
-	exports com.s8.blocks.bytes.demos.d0;
-	
-	
-	requires transitive com.s8.api;
+
+
 
 }
