0b49a8cfac219294157774496113ed8b367b15a4
Pierre Convert <pierreconvert@macbook-pro-6.home>
2023-11-28 12:32:47 +0100

    new build infrastructure

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 const ROOT = build({
 	module: "com.s8.core.io.bytes",
-	dependencies: [ "/Users/pc/qx/git/com.s8.stack/modules/s8-api.jar"],
-	jar: "/Users/pc/qx/git/com.s8.stack/modules/s8-core-io-bytes.jar",
+	dependencies: [ "S8-api"],
+	target: "S8-core-io-bytes",
 });
